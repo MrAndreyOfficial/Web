dff4aa608d92f4a1bf7ab65737f317b8a77559e4
MrAndreyOfficial <mrandreyofficial@gmail.com>
2024-10-07 01:37:10 +0500

    todo-list

---
+++ todo-list/README.md
@@ -0,0 +1,22 @@
+# Frameworks
+[Vue](https://vuejs.org/)
+[Bulma](https://bulma.io)
+
+## Screenshots
+![main](./screenshots/main.png)
+![added_todos](./screenshots/added_todos.png)
+
+## About project
+You can:
+1. Add todos
+2. Remove todos (by clicking on them)
+
+### Project Setup
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+```sh
+npm run dev
+```
---
+++ todo-list/src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+  import { ref } from 'vue';
+
+  const todos = ref([]);
+
+  const todoName = ref(null);
+
+  function addTodo() {
+    if (!todoName) {
+      alert('Enter a task name');
+      return;
+    }
+
+    todos.value.push(todoName.value);
+
+    todoName.value = null;
+  }
+
+  function removeTodo(id) {
+    todos.value.splice(id, 1);
+  }
+</script>
+
+<template>
+  <div class="mt-4 is-flex is-flex-direction-column is-align-items-center">
+    <input type="text" class="input is-success" placeholder="Todo name" v-model="todoName">
+    <button @click="addTodo" class="button mt-3 is-primary">Add Todo</button>
+
+    <ul class="todos is-flex is-flex-direction-column is-align-items-center mt-3">
+      <li class="todo-name" @click="() => removeTodo(index)" v-for="(todo, index) in todos" :key="index">{{ todo }}</li>
+    </ul>
+  </div>
+</template>
+
+<style>
+  .input {
+    width: 500px;
+  }
+
+  .button {
+    width: 250px;
+  }
+
+  .todo-name {
+    font-size: 24px;
+    cursor: pointer;
+  }
+</style>
