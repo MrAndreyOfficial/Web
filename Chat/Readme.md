70cf56fd8883c58e30065b91e945786de41dc360
MrAndreyOfficial <mrandreyofficial@gmail.com>
2025-01-02 19:25:20 +0500

    SignalR Chat

---
+++ Chat/Readme.md
@@ -0,0 +1,7 @@
+# Used technologies
+1. ### [SignalR](https://dotnet.microsoft.com/en-us/apps/aspnet/signalr)
+2. ### [ASP.NET Core](https://dotnet.microsoft.com/en-us/apps/aspnet)
+
+
+# Screenshots
+![Chat](./Screenshots/App.jpg)
---
+++ Chat/wwwroot/js/chat.js
@@ -0,0 +1,27 @@
+﻿"use strict";
+
+const connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+const sendButtonElement = document.getElementById("sendButton");
+const messageListElement = document.getElementById("messagesList");
+
+const userInputElement = document.getElementById("userInput");
+const messageInputElement = document.getElementById("messageInput");
+
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    const li = document.createElement("li");
+    messageListElement.appendChild(li);
+    li.textContent = `${user} says ${message}`;
+});
+
+connection.start().then(() => sendButtonElement.disabled = false).catch(err => console.error(err.toString()));
+
+sendButtonElement.addEventListener("click", e => {
+    const user = userInputElement.value;
+    const message = messageInputElement.value;
+    connection.invoke("SendMessage", user, message).catch((err) => console.error(err.toString()));
+
+    e.preventDefault();
+});
